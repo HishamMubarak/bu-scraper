7096c604682763f3bdea6dc5819a5b4a0a51d730
HishamMubarak <parengalhisham@gmail.com>
2018-03-04 18:34:54 +0400

    Remove Junk

--- scrape.py
+++ scrape.py
@@ -3,7 +3,6 @@ import csv
 from bs4 import BeautifulSoup
 from csv import writer
 
-# students = ["15XWSB7040", "15XWSB7004", "15XWSB7011"]
 students = []
 prefix = "15XWSB"
 start = 7000
@@ -33,7 +32,6 @@ for index, eachId in enumerate(students):
         if addedHeader == False:
             addedHeader = True
             # ADDING CELL HEADERS. ID, NAME and SUBJECT LIST
-            # ==============================================
             tableHeader = ["ID", "Name"]
             for each in results:
                 headers = each.find("span")
@@ -44,25 +42,16 @@ for index, eachId in enumerate(students):
                 else:
                     tableHeader.append(headers.get_text() + " = " + each.select_one("td:nth-of-type(5)").text)
             with open("results.csv", "w") as csv_file:
-                # csv_writer = writer(csv_file)
-                # csv_writer.writerow(tableHeader)
                 fieldNames = tableHeader
                 writer = csv.DictWriter(csv_file, fieldnames=tableHeader)
                 writer.writeheader()
     
         # GETTING STUDENT DETAILS
-        # ==============================================
-        # eachStudent = []
         studentDetails = soup.find(class_="table-light-green")
-        # eachStudent.append(studentDetails.select("#lblRegisterNumber")[0].text)
-        # eachStudent.append(studentDetails.select("#lblStudentName")[0].text)
         name = studentDetails.select("#lblStudentName")[0].text
 
         # GETTING SUBJECT AND MARKS
-        # ==============================================
         rows = results.find_all("tr")
-        
-
         dataToPush = {"ID":eachId, "Name":name}
         # Getting Subject Details into Array
         for rowIndex,row in enumerate(rows[1:]):
@@ -71,8 +60,6 @@ for index, eachId in enumerate(students):
             row.find(id="gvResults_lblRankDetails_"+str(rowIndex)).decompose()
 
             eachSubject = row.select_one("td:nth-of-type(1)").find("span").text
-            # subjectCode = row.select_one("td:nth-of-type(2)").text
-            # semester = row.select_one("td:nth-of-type(3)").text
             subType = row.select_one("td:nth-of-type(4)").text
             eachMarks = row.select_one("td:nth-of-type(5)").text
 
@@ -83,15 +70,10 @@ for index, eachId in enumerate(students):
                     if eachSubject in eachTitle:
                         if subType in eachTitle:
                             dataToPush[eachTitle] = eachMarks
-                            # rowToWrite = [{"ID":eachId,"name":name,eachTitle:eachMarks}]
-                            # print(rowToWrite)
 
         allStudents.append(dataToPush)
         print(eachId)
-        # print(allStudents)
 
-# print(allStudents)
-# print(type(fieldNames))
 with open('results.csv', 'w', newline='') as csvFile:
     writer = csv.DictWriter(csvFile, fieldnames=fieldNames)
     writer.writeheader()
